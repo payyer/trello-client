ff26771e80e812178fbd6accdd022e388289a67c
payyer <quocanhle112@gmail.com>
2025-04-29 14:46:36 +0700

    Setup Light/Dark mode #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import Button from '@mui/material/Button'
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import Typography from '@mui/material/Typography';
+
+import { useColorScheme } from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+
 function App() {
   return (
     <>
-      <div>Lê Quốc Anh</div>
+      <div>Lê Quốc Anh</div> 
+      <ModeToggle/>
       <Typography variant='body2' color={"text.secondary"}>Hello Worlds</Typography>
       <AcUnitIcon/>
       <Button variant="text">Text</Button>
