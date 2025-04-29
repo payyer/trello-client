9f9a2e3568e73d96385d0345a1f75540e43c7ceb
payyer <quocanhle112@gmail.com>
2025-04-29 14:14:21 +0700

    config theme mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Button from '@mui/material/Button'
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import Typography from '@mui/material/Typography';
 function App() {
   return (
     <>
       <div>Lê Quốc Anh</div>
+      <Typography variant='body2' color={"text.secondary"}>Hello Worlds</Typography>
       <AcUnitIcon/>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
